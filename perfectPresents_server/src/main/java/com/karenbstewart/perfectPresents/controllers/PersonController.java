e067f703b657cd6a0da8d0b2b4f09aa60a1379c5
Karen Stewart <kastewart2002@googlemail.com>
2021-12-13 15:22:13 +0000

    working on the person controller

--- perfectPresents_server/src/main/java/com/karenbstewart/perfectPresents/controllers/PersonController.java
+++ perfectPresents_server/src/main/java/com/karenbstewart/perfectPresents/controllers/PersonController.java
@@ -1,9 +1,24 @@
 package com.karenbstewart.perfectPresents.controllers;
 
+import com.karenbstewart.perfectPresents.models.Person;
+import com.karenbstewart.perfectPresents.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PersonController {
+    
+    @Autowired
+    PersonRepository personRepository;
 
+    @GetMapping(value = "/people")
+    public ResponseEntity<List<Person>> getAllPeople(){
+        return new ResponseEntity<List<Person>>(personRepository.findAll(), HttpStatus.OK);
+    }
 
 }
