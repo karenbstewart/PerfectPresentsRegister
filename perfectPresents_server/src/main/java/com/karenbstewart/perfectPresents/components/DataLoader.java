f4f24bbd4c7c9a055127b8eb118e95eddbcb918e
Karen Stewart <kastewart2002@googlemail.com>
2021-12-13 15:57:54 +0000

    changing application properties

--- perfectPresents_server/src/main/java/com/karenbstewart/perfectPresents/components/DataLoader.java
+++ perfectPresents_server/src/main/java/com/karenbstewart/perfectPresents/components/DataLoader.java
@@ -1,4 +1,40 @@
 package com.karenbstewart.perfectPresents.components;
 
+import com.karenbstewart.perfectPresents.models.Gift;
+import com.karenbstewart.perfectPresents.models.Person;
+import com.karenbstewart.perfectPresents.repositories.GiftRepository;
+import com.karenbstewart.perfectPresents.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DataLoader {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @Autowired
+    GiftRepository giftRepository;
+
+    public DataLoader() {
+    }
+
+    public void run(ApplicationArguments args) {
+        Person Aiden = new Person("Aiden", "Stewart", "aiden@gmail.com");
+        personRepository.save(Aiden);
+        Person Sean = new Person("Séan", "Stewart", "sean@gmail.com");
+        personRepository.save(Sean);
+
+        Gift Sloth_oodie = new Gift("sloth ooodie", "https://theoodie.co.uk/products/sloth-oodie?awc=23760_1639409335_04569aa40e930e8f248525fbc54f1b86", 59.00, Aiden);
+        giftRepository.save(Sloth_oodie);
+        Gift Sloth_bottle =  new Gift("sloth bottle", "https.thesothbottlecompany", 25.45, Aiden);
+        giftRepository.save(Sloth_bottle);
+
+        Gift raspberryPi = new Gift("Raspberry Pi 3B+", "https://techstuff", 34.94, Sean);
+        giftRepository.save(raspberryPi);
+        Gift fluffypenguin = new Gift("toy penguin", "https://Amazon", 26.00, Sean);
+
+
+    }
 }
--- perfectPresents_server/src/main/java/com/karenbstewart/perfectPresents/controllers/GiftController.java
+++ perfectPresents_server/src/main/java/com/karenbstewart/perfectPresents/controllers/GiftController.java
@@ -1,4 +1,12 @@
 package com.karenbstewart.perfectPresents.controllers;
 
+import com.karenbstewart.perfectPresents.repositories.GiftRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GiftController {
+    @Autowired
+    GiftRepository giftRepository;
+
 }
