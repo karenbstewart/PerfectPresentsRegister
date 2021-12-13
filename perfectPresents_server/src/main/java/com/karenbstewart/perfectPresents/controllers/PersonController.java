c3a5de2a78e0d3dc4645a6d770f785af87ad2a42
Karen Stewart <kastewart2002@googlemail.com>
2021-12-13 17:48:20 +0000

    mappings in the person controller defined

--- perfectPresents_server/src/main/java/com/karenbstewart/perfectPresents/controllers/PersonController.java
+++ perfectPresents_server/src/main/java/com/karenbstewart/perfectPresents/controllers/PersonController.java
@@ -5,8 +5,7 @@ import com.karenbstewart.perfectPresents.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +19,31 @@ public class PersonController {
     public ResponseEntity<List<Person>> getAllPeople(){
         return new ResponseEntity<List<Person>>(personRepository.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping(value = "/people/{id}")
+    public ResponseEntity getShip(@PathVariable Long id){
+        return new ResponseEntity<>(personRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/people")
+    public ResponseEntity<Person> createPerson(@RequestBody Person person){
+        personRepository.save(person);
+        return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+
+    @PatchMapping(value = "/people/{id}")
+    public ResponseEntity<Person> updatePerson(@RequestBody Person person){
+        personRepository.save(person);
+        return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+
+    // get one is depreciated ?? try using getById
+    @DeleteMapping(value = "/people/{id}")
+    public ResponseEntity<Person> deletePerson(@PathVariable Long id){
+        Person found = personRepository.getById(id);
+        personRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
 
 }
